1a68398044d57f6d872c09c1d43821668a44498f
georgejeswin <jeswinmyladoor@gmail.com>
2021-06-06 14:54:54 +0530

    landingpage header footer terms privacy updated

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -24,7 +24,7 @@ const LandingPage = () => {
   return (
     <div className="landingPage">
       <div className="landingPage__contents">
-        <h3>The Best Educational Consultancy</h3>
+        <h3>The Best Educational Services</h3>
         <h1>Opera International</h1>
         <button
           onClick={() => {
