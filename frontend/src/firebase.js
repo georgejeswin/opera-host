05600f7ba7f8c6019eb448ca5a6104e1161bc04a
Jeswin George <jeswingeorge@Jeswins-MacBook-Air.local>
2021-07-13 17:09:42 -0700

    analytics

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.analytics();
 
 export const auth = firebase.auth();
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
